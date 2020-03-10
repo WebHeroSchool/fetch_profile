dff7eb16d2ee66119bd87a109f9dcc8718dfa2ac
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-03-11 00:11:29 +0300

    new changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const url = window.location.toString();
-let getUserName = function(url) {  //получаем имя пользователя из url, который далее будем использовать в функции
-    let arrFromUrl = url.split('='); //разбиваем строку на массив строк путем разделения строки символом '=', получается 2 элемента в массиве (0-первая часть url, 1- вторая часть url)
-    let userName = arrFromUrl[1]; // имя пользователя становится вторым элементом массива
-    if (userName  == undefined) { // это типа по умолчанию страница, для того, чтобы из файла index.html открылась страница с пользователем, и не надо было userName вручную вбивать
+let getUserName = function(url) {
+    let arrFromUrl = url.split('=');
+    let userName = arrFromUrl[1];
+    if (userName  == undefined) {
         userName = 'galigalinochka';
     }
     return userName;
@@ -13,30 +13,39 @@ let name = getUserName(url);
 fetch(`https://api.github.com/users/${name}`)
     .then(response => {
         if (response.status != 200) {
-            alert('Информация о пользователе не доступна');
+            return null;
         } else {
             return response.json();
         }
     })
     .then(json => {
-        let getImage = () => {
-            let photo = document.querySelector('.photo');
-            photo.src = json.avatar_url;
-        }
-        let getName = () => {
-            let user = document.querySelector('.name');
-            let link = document.querySelector('.link');
-            user.innerHTML = json.name;
-            link.href = json.html_url;
-        }
-        let getInfo = () => {
-            let info = document.querySelector('.info');
-            info.innerHTML = json.bio;
-        }
+            let getImage = () => {
+                let photo = document.querySelector('.photo');
+                photo.src = json.avatar_url;
+                //photo.innerHTML = photo;
+            }
+
+            let getName = () => {
+                let user = document.querySelector('.name');
+                let link = document.querySelector('.link');
+                let theName = json.name;
+                user.innerHTML = theName;
+                link.href = json.html_url;
+                if (theName === null) {
+                    user.innerHTML = json.login;
+                }
+            }
+
+            let getInfo = () => {
+                let info = document.querySelector('.info');
+                info.innerHTML = json.bio;
+            }
 
 
-        getImage();
-        getName();
-        getInfo();
+            getImage();
+            getName();
+            getInfo();
     })
+    .catch(err => alert('Информация о пользователе не доступна'));
+
 
