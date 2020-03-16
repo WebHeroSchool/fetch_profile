b63dfbbd28180ec44ac264503d9ada6fa7f2217b
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-03-16 21:53:41 +0300

    changes

--- script.js
+++ script.js
@@ -8,22 +8,20 @@ let getUserName = function(url) {
 }
 const getDate = new Promise((resolve, reject) => {
     setTimeout(() => {
-            let date = new Date();
-            let time = document.getElementById('time');
-            time.innerHTML = date;
-            date ? resolve(date) : reject('Время не определено')},
+        let date = new Date();
+        let time = document.getElementById('time');
+        time.innerHTML = date;
+        date ? resolve(date) : reject('Время не определено')},
         3000)
 })
-console.log(getDate);
+
 let name = getUserName(url);
-console.log(name);
 
 let getProfileInfo = fetch(`https://api.github.com/users/${name}`)
-console.log(getProfileInfo);
+
 Promise.all([getDate, getProfileInfo])
     .then(([date, request]) => {
         let res = request.json();
-        console.log(res);
         if (request.status != 200) {
             return null;
         } else {
@@ -40,7 +38,6 @@ Promise.all([getDate, getProfileInfo])
             let getName = () => {
                 let user = document.querySelector('.name');
                 let link = document.querySelector('.link');
-
                 user.innerHTML = theName;
                 link.href = json.html_url;
                 if (theName === null) {
@@ -51,7 +48,11 @@ Promise.all([getDate, getProfileInfo])
             let getInfo = () => {
                 let info = document.querySelector('.info');
                 info.innerHTML = json.bio;
-            } 
+            }
+
+            let preloader = document.getElementById('preloader');
+            preloader.classList.add('hidden');
+
             getImage();
             getName();
             getInfo();
